2cbc437144c8970afdaaea3b70e4f7edcf66b43d
James <jameskra123@gmail.com>
2021-04-07 23:31:20 +0700

    Remove ejs-mate

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Product = require('./models/product');
-const engine = require('ejs-mate');
 const session = require('express-session');
 
 const app = express();
@@ -31,7 +30,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.engine('ejs', engine);
 
 app.use(function (req, res, next) {
   res.locals.session = session;
